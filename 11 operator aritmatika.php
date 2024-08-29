e1a54433edb735e3a10d65ddd59007e799eb2b88
mafriansyah <ryanksp34@gmail.com>
2024-08-29 20:22:28 +0700

    11 operator aritmatika

---
+++ 11 operator aritmatika.php
@@ -0,0 +1,37 @@
+<?php
+$a = 4;
+$b = 3;
+
+// penjumlahan
+$c = $a + $b;
+echo "$a + $b = $c";
+echo "<hr>";
+
+// pengurangan
+$c = $a - $b;
+echo "$a - $b = $c";
+echo "<hr>";
+
+// Perkalian
+$c = $a * $b;
+echo "$a * $b = $c";
+echo "<hr>";
+
+// Pembagian
+$c = $a / $b;
+echo "$a / $b = $c";
+echo "<hr>";
+
+// modulus Sisa bagi
+$c = $a % $b;
+echo "$a % $b = $c";
+echo "<hr>";
+
+// Pangkat
+$c = $a ** $b;
+echo "$a ** $b = $c";
+echo "<hr>";
+
+$hasil = (4 + 4) * 3; // Hasilnya adalah 24, bukan 20
+echo $hasil;
+echo "<hr>";
