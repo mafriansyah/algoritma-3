7496cdd701d15be6defe7769f3c9b1642a2be0ff
mafriansyah <ryanksp34@gmail.com>
2024-08-29 11:34:00 +0700

    10 print_r array

---
+++ 10 print_r array.php
@@ -0,0 +1,20 @@
+<?php
+$array = array(
+    'name' => 'M AFRIANSYAH',
+    'age' => 16,
+    'skills' => array('PHP', 'JavaScript', 'HTML')
+);
+
+print_r($array);
+
+print "<p>";
+
+$array = array(
+    'name' => 'FACHRI RISKI',
+    'age' => 45,
+    'skills' => array('PHP', 'JavaScript', 'HTML')
+);
+
+var_dump($array);
+
+print "<p>";
