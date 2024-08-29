85cf251642c6aa220a0f421b4865ca98b36e4d72
mafriansyah <ryanksp34@gmail.com>
2024-08-29 10:44:29 +0700

    05 vardump bertipe objek

---
+++ 05 vardump bertipe objek.php
@@ -0,0 +1,14 @@
+<?php
+class sepatu {
+    public $merk;
+    public $model;
+}
+
+$sepatu_baru = new Mobil();
+$sepatu_baru->merk = "converse";
+$sepatu_baru->model = "vans";
+
+var_dump($sepatu_baru);
+?>
+
+print_r("<p><strong>by :M Afriansyah");
