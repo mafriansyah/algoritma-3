40da1d8bdf109682d2548056b292ceaf02aa1738
mafriansyah <ryanksp34@gmail.com>
2024-08-29 20:44:03 +0700

    12 operator penugasan

---
+++ 12 operator penugasan.php
@@ -0,0 +1,23 @@
+<?php
+
+$speed = 80;
+$speed2 = 20;
+
+// ini opertor aritmatika
+$speed = $speed + 20;
+
+// maka nilai speed akan samadengan 80 + 20 = 100
+
+// ini operator penugasan
+$speed += 20;
+
+// sekarang nilai speed akan menjadi 90 + 20 = 110
+print($speed)."<hr>";
+
+$speed2 = $speed2 + 20;
+// dan
+$speed2 += 20;
+
+print($speed2);
+
+print_r("<p><strong>by :M Afriansyah");
