976a6a689594fabe92c82e95ec073843679d1135
mafriansyah <ryanksp34@gmail.com>
2024-08-29 21:40:29 +0700

    18 operator ternary

---
+++ 18 operator ternary.php
@@ -0,0 +1,11 @@
+<?php
+
+$suka = true;
+
+// menggunakan operator ternary
+$jawab = $suka ? "iya": "tidak";
+
+// menampilkan jawaban
+echo $jawab = "iya";
+
+print_r("<p><strong>by :M Afriansyah");
