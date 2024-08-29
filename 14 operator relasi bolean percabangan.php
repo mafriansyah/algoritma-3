c51b7088d2ed6b05cd9ad1e6c76fce9039ed5de1
mafriansyah <ryanksp34@gmail.com>
2024-08-29 21:09:58 +0700

    14 operator relasi bolean percabangan

---
+++ 14 operator relasi bolean percabangan.php
@@ -0,0 +1,12 @@
+<?php
+$total_belanja = 15000000;
+
+if($total_belanja > 10000000){
+    echo "Anda dapat kijang!";
+}
+
+if($total_belanja < 0){
+    echo "login";
+}
+print("<hr>");
+var_dump("hallo guys");
