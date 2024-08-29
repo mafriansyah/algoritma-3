e074d64dc81e65c03704471cb7caeb6e670ef5c5
mafriansyah <ryanksp34@gmail.com>
2024-08-29 10:37:59 +0700

    04 Menggunakan var_dump

---
+++ 04 Menggunakan var_dump.php
@@ -0,0 +1,11 @@
+<?php
+$variabel_string = "Halo rusli!";
+$variabel_integer = 123;
+$variabel_array = array(1, 2, "tiga", 4.5);
+
+var_dump($variabel_string);
+var_dump($variabel_integer);
+var_dump($variabel_array);
+?>
+
+print_r("<p><strong>by :M Afriansyah");
