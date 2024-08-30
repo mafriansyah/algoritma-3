85bf97f6f46b4cdbbd148330661a5dbd7331cc84
mafriansyah <ryanksp34@gmail.com>
2024-08-30 09:24:08 +0700

    34 Operasi Matematika Dasar

---
+++ 34 Operasi Matematika Dasar.php
@@ -0,0 +1,18 @@
+<?php
+$angka1 = 20;
+$angka2 = 5;
+
+// Penambahan
+echo $angka1 + $angka2; // Hasil: 25
+
+// Pengurangan
+echo $angka1 - $angka2; // Hasil: 15
+
+// Perkalian
+echo $angka1 * $angka2; // Hasil: 100
+
+// Pembagian
+echo $angka1 / $angka2; // Hasil: 4
+
+
+print_r("<p><strong>by :M Afriansyah");
