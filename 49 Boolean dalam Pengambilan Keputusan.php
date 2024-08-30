f11f7ca8acea60d78a30cfe0ffd7b3810872a2d0
mafriansyah <ryanksp34@gmail.com>
2024-08-30 10:34:44 +0700

    49 Boolean dalam Pengambilan Keputusan

---
+++ 49 Boolean dalam Pengambilan Keputusan.php
@@ -0,0 +1,10 @@
+<?php
+$login_sukses = true;
+
+if ($login_sukses) {
+    echo "Selamat datang pengguna!";
+} else {
+    echo "Login gagal, silakan coba lagi.";
+}
+
+print_r("<p><strong>by :M Afriansyah");
