85fcaac76329f64316eb6220f46f5f10055d813c
mafriansyah <ryanksp34@gmail.com>
2024-08-30 10:27:21 +0700

    47 pernyataan elseif

---
+++ 47 pernyataan elseif.php
@@ -0,0 +1,11 @@
+<?php
+$nilai = 90;
+if ($nilai >= 90) {
+  echo "Nilai kamu A";
+} elseif ($nilai >= 80) {
+  echo "Nilai kamu B";
+} else {
+  echo "Nilai kamu C";
+}
+
+print_r("<p><strong>by :M Afriansyah");
