f987759e26f0ea7fc84ba88de4974c1b2c7c18e2
mafriansyah <ryanksp34@gmail.com>
2024-08-30 10:30:15 +0700

    48 pernyataan switch

---
+++ 48 pernyataan switch.php
@@ -0,0 +1,15 @@
+<?php
+$grade = 'A';
+switch ($grade) {
+  case 'A':
+    echo "Luar biasa!";
+    break;
+  case 'B':
+  case 'C':
+    echo "Bagus!";
+    break;
+  default:
+    echo "Tetap semangat!";
+}
+
+print_r("<p><strong>by :M Afriansyah");
